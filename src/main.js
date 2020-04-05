f3bf742e5ea54cf4cd56e8da9cdae3ef4844fcb0
Albert <sab4856@gmail.com>
2020-04-05 15:35:51 +0800

    建立環境 and 安裝套件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import 'bootstrap';
+
 import App from './App.vue';
 import router from './router';
 
+Vue.use(VueAxios, axios);
+axios.defaults.withCredentials = true;
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav class="l-navbar l-navbar--fixedTop container px-0">
+        <h1><a class="l-navbar__logo h-text-4" href="#">WheelTalk</a></h1>
+        <ul class="l-navbar__menu">
+          <li class="l-navbar__menu__item"><a href="#">首頁</a></li>
+          <li class="l-navbar__menu__item"><a href="">產品</a></li>
+          <li class="l-navbar__menu__item"><a href="#">作者</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <div class="c-hero">
+        <video :src="require('@/assets/video/headerVideo.mp4')" type="video/mp4"
+        autoplay="true" muted="true" loop="true"></video>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
